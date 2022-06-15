5d8606a3a0341580f3cb29e5a0176c89438c60cf
Harsh <Hvp6848@gmail.com>
2022-06-15 14:43:33 +0530

    Added routing and some features

--- first.js
+++ first.js
@@ -14,60 +14,71 @@ const url = require("url");
 
 // Server
 
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 
-const replaceFunc= (tempCard,data)=>{
-let obj=tempCard.replace(/{%Logo%}/g,data.image); ``
-obj=obj.replace(/{%ProductName%}/g,data.productName);
-obj=obj.replace(/{%Quantity%}/g,data.quantity);
-obj=obj.replace(/{%Price%}/g,data.price);
-obj=obj.replace(/{%id%}/g,data.id);
-obj=obj.replace(/{%Place%}/g,data.from);
-obj=obj.replace(/{%Nutrient%}/g,data.nutrients);
-obj=obj.replace(/{%Number%}/g,data.quantity);
-obj=obj.replace(/{%Price%}/g,data.price);
-obj=obj.replace(/{%Description%}/g,data.description);
+const replaceFunc = (tempCard, data) => {
+  let obj = tempCard.replace(/{%Logo%}/g, data.image);
+  obj = obj.replace(/{%ProductName%}/g, data.productName);
+  obj = obj.replace(/{%Quantity%}/g, data.quantity);
+  obj = obj.replace(/{%Price%}/g, data.price);
+  obj = obj.replace(/{%id%}/g, data.id);
+  obj = obj.replace(/{%Place%}/g, data.from);
+  obj = obj.replace(/{%Nutrient%}/g, data.nutrients);
+  obj = obj.replace(/{%Number%}/g, data.quantity);
+  obj = obj.replace(/{%Price%}/g, data.price);
+  obj = obj.replace(/{%Description%}/g, data.description);
 
-if(!data.organic) {obj=obj.replace(/{%Not Organic%}/g,'not-organic');}
+  if (!data.organic) {
+    obj = obj.replace(/{%NotOrganic%}/g, "not-organic");
+  }
 
-console.log(obj);
-return obj;
-
-}
-
+  // console.log(obj);
+  return obj;
+};
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const responseData = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathname = req.url;
 
+  const { pathname, query } = url.parse(req.url, true);
+  
   //Overview section
-
+  
   if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
-    const content=responseData.map(e=>replaceFunc(tempCard,e)).join('');
-    const output=tempOverview.replace('{%Cards%}',content);
+    const content = responseData.map((e) => replaceFunc(tempCard, e)).join("");
+    const output = tempOverview.replace("{%Cards%}", content);
+    // console.log(responseData);
     res.end(output);
   }
 
   //Product section
 
   else if (pathname === "/product") {
-    res.end("This is product");
+    const product=responseData[query.id];
+    const output=replaceFunc(tempProduct,product);
+    res.end(output);
   }
 
   //API section
-  
   else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application-json" });
     res.end(data);
   }
-  
+
   //Not Found section
-  
   else res.end("Page not found");
 });
 server.listen(8000, "127.0.0.1", () => {
